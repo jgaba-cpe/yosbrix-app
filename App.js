24ffc967169c8776b6a6a0392ea881267bc8f84f
jgaba-cpe <qjpgaba@tip.edu.ph>
2023-02-16 15:24:31 +0800

    second commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi Mom!</Text>
+      <Text>Hi Mom and Dad!</Text>
       <StatusBar style="auto" />
     </View>
   );
